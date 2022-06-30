91c4d810f4c51ba4c8b0a595ce45456c53001505
G Brezeanu <gbrezeanu@protonmail.com>
2022-06-30 23:45:26 +0300

    deploy test

--- src/flask_entrypoint.py
+++ src/flask_entrypoint.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, avem Python intr-un Docker container! - Test'
+    return 'Hey, avem Python intr-un Docker container! - Test!'
 
 
 if __name__ == '__main__':
